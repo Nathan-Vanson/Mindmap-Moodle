ca907918b8668fcc28c937cec09a0155c697b3d5
Juice <Nathan.Vanson@gmail.com>
2022-06-29 18:21:29 +0200

    Remove old files

--- src/popup.js
+++
@@ -1,9 +0,0 @@
-function disabled() {
-  document.getElementById("popup-content-disabled").disabled = true;
-}
-
-function enabled() {
-  document.getElementById("popup-content-enabled").disabled = false;
-}
-
-
